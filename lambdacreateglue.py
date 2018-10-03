f76bee953a55d8921e8927b1e67d5c7d57a61240
Ganondorfz <42991528+Ganondorfz@users.noreply.github.com>
2018-10-03 14:26:23 +1000

    Added passing of .tar.gz or .zip to Glue
    Lambda reads the triggerid to see if the event from S3 is 'zip' related or 'tar.gz/.tgz' and passes this to Glue.

--- lambdacreateglue.py
+++ lambdacreateglue.py
@@ -6,14 +6,21 @@ glue = boto3.client('glue')
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    print(key)    
-
+    print(key)
+    #i am using the name of the S3 notification to indicate if it is finding .zip suffix or .tgz/.tar.gz 
+    triggerid = urllib.parse.unquote_plus(event['Records'][0]['s3']['configurationId'], encoding='utf-8')
+    print(triggerid)
+    #if zipfile, triggid = 'zip'    
+    destbucket = 'YOURDESTINATIONBUCKETHERE'
+    
     try:
         newJobRun = glue.start_job_run(
             JobName = 'dupe',
             Arguments = {
                 '--bucket':bucket,
                 '--key':key,
+                '--destbucket':destbucket,
+                '--filetype':triggerid
             }
             )
         print("Successfully created unzip job")    
@@ -21,4 +28,4 @@ def lambda_handler(event, context):
     except Exception as e:
         print(e)
         print('Error starting unzip job for' + key)
-        raise e         
+        raise e       
